02aa3e313eee1eb3e1db58abe87c765977772b3e
nutan2247 <nutan2247@gmail.com>
2025-05-07 22:50:52 +0530

    init code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Platform } from '@ionic/angular';
+import { FirebaseX } from '@awesome-cordova-plugins/firebase-x/ngx';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,18 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private firebaseX: FirebaseX
+  ) {
+    this.platform.ready().then(() => {
+      this.firebaseX.getToken().then(token => {
+        console.log("FCM Token:", token);
+      });
+
+      this.firebaseX.onMessageReceived().subscribe(data => {
+        console.log("Push received:", data);
+      });
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { FirebaseX } from '@awesome-cordova-plugins/firebase-x/ngx';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    FirebaseX
   ],
 });
